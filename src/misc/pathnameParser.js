ae9a552d6d188a163f9a34043c84a981b3effcb4
egorkqq <egorkqq@gmail.com>
2019-09-29 23:31:53 +0500

    feat: set titles for each page

--- src/misc/pathnameParser.js
+++ src/misc/pathnameParser.js
@@ -4,18 +4,30 @@ export const pathnameParser = (pathname) => {
     pathname.includes('/language/') ||
     pathname.includes('/continent/')
   ) {
+    global.document.title = `Geo:${pathname.slice(-2)}`;
+
     return pathname.slice(-2);
   }
   switch (pathname) {
     case '/':
+      global.document.title = `Geo:Main`;
+
       return 'Main';
     case '/favourites':
+      global.document.title = `Geo:Favourites`;
+
       return 'Favourites';
     case '/all':
+      global.document.title = `Geo:All`;
+
       return 'All';
     case '/search':
+      global.document.title = `Geo:Search`;
+
       return 'Search';
     default:
+      global.document.title = `Geo:Main`;
+
       return 'Main';
   }
 };
